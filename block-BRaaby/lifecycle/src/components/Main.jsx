cc79f472261dc6fbfc0b003558cc9b6a8333d1c7
vishalgoswami12 <vishalgoswami2250@gmail.com>
2022-11-29 11:26:17 +0530

    random

--- block-BRaaby/lifecycle/src/components/Main.jsx
+++ block-BRaaby/lifecycle/src/components/Main.jsx
@@ -6,6 +6,7 @@ class Main extends React.Component {
         super();
         this.state={
             data: null,
+            tab:"name"
         }
     }
     componentDidMount(){
@@ -17,18 +18,22 @@ class Main extends React.Component {
         .then((res)=>res.json())
         .then((data)=> 
             this.setState({
-                data: data.results[0]
+                data: data.results[0],
+                tab: 'name'
             })
         ) 
     }
 
+    setTab=(tab)=>{
+        this.setState({tab})
+    }
+
 
     render(){
-        const { data } = this.state;
+        const { data = {}, tab = '' } = this.state;
         if(!data){
             return <h1>Loading.....</h1>
         }
-        console.log(data, 'check')
         const { email, picture: { medium },name,login,phone} = data;
         return(
         <>
@@ -38,17 +43,33 @@ class Main extends React.Component {
                 <img src={medium} alt={medium} />
                 </div>
                 
-                <h3 className="center">My name is</h3>
-                <p className="center">{name.title} <span>{name.first}</span> <span>{name.last}</span></p>
-                <p className="center">Username:{login.username}</p>
-                <p className="center">Password:{login.password}</p>
-                <h2 className="center">{email}</h2>
+                <h3 className="center">My {tab} is</h3>
+                { tab === 'name' && (
+                    <p className="center">{name.title} <span>{name.first}</span> <span>{name.last}</span></p>
+                )}
+                {
+                    tab === 'user' && (
+                        <p className="center">{login.username}</p>
+                    )
+                }
+                 {
+                    tab === 'phone' && (
+                        <p className="center">{phone}</p>
+                    )
+                }
+                {tab === 'lock' && (
+                    <p className="center">{login.password}</p>
+                )}
+                {tab === 'email' && (
+                    <h2 className="center">{email}</h2>
+                )}
                 <div className="flex width">
-                <i className="fa-solid fa-envelope" id="name" ></i>
-                <i class="fa-solid fa-user"></i>
+                <i className="fa-solid fa-envelope" id="name" onClick={()=> this.setTab('email')}></i>
+                {/* <button onClick={()=> this.setTab('email')}>mail</button> */}
+                <i class="fa-solid fa-user" onClick={()=> this.setTab('user')}></i>
                 <i class="fa-solid fa-sensor-on"></i>
-                <i class="fa-solid fa-phone"></i>
-                <i class="fa-solid fa-lock"></i>
+                <i class="fa-solid fa-phone" onClick={()=> this.setTab('phone')}></i>
+                <i class="fa-solid fa-lock" onClick={()=> this.setTab('lock')}></i>
                 </div>
                  {/* <p>{gender}</p>
               <p>{email}</p>  */}
